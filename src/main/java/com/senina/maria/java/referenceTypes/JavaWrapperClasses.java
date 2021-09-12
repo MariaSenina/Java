93c54dcdb7208fdf7708b18696f8db11943a8145
Maria <maria.senina15@gmail.com>
2021-09-12 18:22:47 -0400

    Reference Types: Create instances of wrapper classes

---
+++ src/main/java/com/senina/maria/java/referenceTypes/JavaWrapperClasses.java
@@ -0,0 +1,20 @@
+package com.senina.maria.java.referenceTypes;
+
+public class JavaWrapperClasses {
+
+    public static void main(String[] args) {
+        //creating instances of wrapper classes using "new"
+        Integer integer = new Integer(5);
+        Integer integer2 = new Integer("5");
+
+        //creating instances of wrapper classes using .valueOf
+        Integer integer1 = Integer.valueOf(5);
+        Integer integer3 = Integer.valueOf("5");
+        // ^ recommended; check if there is already an object with the given value and, if so, it doesn't create a new
+        //object, merely a new pointer to the same object (it's okay because wrapper classes are immutable)
+
+        //the difference between the two creating methods
+        System.out.println(integer == integer2); //compares where the values are stored
+        System.out.println(integer1 == integer3); //compares the values themselves
+    }
+}
