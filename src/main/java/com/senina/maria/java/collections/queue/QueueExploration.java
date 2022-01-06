678e558a646c9d2dd26adbb48e6e19f1f941e501
Maria <maria.senina15@gmail.com>
2022-01-06 16:11:59 -0500

    Collections: PriorityQueue poll() method

--- src/main/java/com/senina/maria/java/collections/queue/QueueExploration.java
+++ src/main/java/com/senina/maria/java/collections/queue/QueueExploration.java
@@ -1,11 +1,20 @@
 package com.senina.maria.java.collections.queue;
 
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class QueueExploration {
     public static void main(String[] args) {
         Queue<String> queue = new PriorityQueue<>();
+
+        queue.offer("Apple");
+        queue.addAll(List.of("Zebra", "Monkey", "Cat"));
+
+        System.out.println("Queue: " + queue); // -> sorted
+
+        queue.poll(); // -> takes the top element of the queue (reads and removes)
+        System.out.println("After poll(): " + queue);
     }
 
     //A queue is used when we want to arrange things in the order we want to process them
