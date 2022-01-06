2ed98601140790a2daa4a082b007ab69e6aa720f
Maria <maria.senina15@gmail.com>
2022-01-06 16:06:14 -0500

    Collections: Queue basics

---
+++ src/main/java/com/senina/maria/java/collections/queue/QueueExploration.java
@@ -0,0 +1,18 @@
+package com.senina.maria.java.collections.queue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueExploration {
+    public static void main(String[] args) {
+        Queue<String> queue = new PriorityQueue<>();
+    }
+
+    //A queue is used when we want to arrange things in the order we want to process them
+    // (similar to a to-do list)
+    //Queue extends Collection
+    // -> supports all the collection methods
+    // -> has additional methods
+    //Implementations:
+    // -> Priority Queue: elements are sorted in a natural order (changing the order is also possible)
+}
