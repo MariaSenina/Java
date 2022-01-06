704cdffe64fc70ae2a7de42e349e919122695eeb
Maria <maria.senina15@gmail.com>
2022-01-06 14:22:46 -0500

    Collections: Hash table data structure notes

--- src/main/java/com/senina/maria/java/collections/set/SetExploration.java
+++ src/main/java/com/senina/maria/java/collections/set/SetExploration.java
@@ -12,4 +12,22 @@ public class SetExploration {
     //Set does not care about the position:
     // -> No indexes
     // -> The insert order is not respected
+
+    //HASH TABLE:
+
+    //Tries to combine:
+    // -> the fixed positions (like an array)
+    // -> the advantages of a LinkedList
+
+    //Positions are called "buckets"
+    //Elements are stored into "buckets" through the use of a hashing function.
+    //Hashing function example -> mod 13
+    // -> divide an element by 13, take the remainder and put the element in that place. I.e. 15 % 13 is 2 -> 2nd place
+    //Advantages:
+    // -> easy insertion
+    // -> easy search
+    // -> easy deletion
+    //NOTE: the efficiency of your hash table will always depend on the efficiency of your hashing function
+
+    //Implementation of the hashing function is always done through a hashcode() method
 }
