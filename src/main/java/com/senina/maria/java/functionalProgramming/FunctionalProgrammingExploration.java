963d28236395070f917af3ba71af002fccd6e903
Maria <maria.senina15@gmail.com>
2022-01-07 17:11:01 -0500

    Functional Programming: Print elements ending with 'at'

--- src/main/java/com/senina/maria/java/functionalProgramming/FunctionalProgrammingExploration.java
+++ src/main/java/com/senina/maria/java/functionalProgramming/FunctionalProgrammingExploration.java
@@ -7,19 +7,42 @@ public class FunctionalProgrammingExploration {
         List<String> list = List.of("Apple", "Banana", "Cat", "Dog");
 
         System.out.println("Basic:");
+        System.out.print("Print elements: ");
         printBasic(list);
+        System.out.println();
+        System.out.print("Print elements ending with 'at': ");
+        printBasicWithFilter(list);
+        System.out.println();
 
         System.out.println("Functional:");
+        System.out.print("Print elements: ");
         printWithFP(list);
+        System.out.println();
+        System.out.print("Print elements ending with 'at': ");
+        printWithFPWithFilter(list);
+        System.out.println();
     }
 
     private static void printBasic(List<String> list) {
         for (String string : list) {
-            System.out.println(string);
+            System.out.print(string + " ");
         }
     }
 
     private static void printWithFP(List<String> list) {
-        list.stream().forEach(element -> System.out.println("element - " + element));
+        list.stream().forEach(element -> System.out.print("element - " + element + " "));
+    }
+
+    private static void printBasicWithFilter(List<String> list) {
+        for (String string : list) {
+            if(string.endsWith("at")) {
+                System.out.print(string + " ");
+            }
+        }
+    }
+    private static void printWithFPWithFilter(List<String> list) {
+        list.stream().
+                filter(element -> element.endsWith("at")).
+                forEach(element -> System.out.print("element - " + element + " "));
     }
 }
