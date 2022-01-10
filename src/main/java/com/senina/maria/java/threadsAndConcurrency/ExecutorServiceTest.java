6166af2a8869f593a658ace50b2ec9f88ead8bfb
Maria <maria.senina15@gmail.com>
2022-01-10 17:54:45 -0500

    Threads: ExecutorService, multiple threads + refactor

--- src/main/java/com/senina/maria/java/threadsAndConcurrency/ExecutorServiceTest.java
+++ src/main/java/com/senina/maria/java/threadsAndConcurrency/ExecutorServiceTest.java
@@ -3,13 +3,32 @@ package com.senina.maria.java.threadsAndConcurrency;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task extends Thread {
+    private int number;
+
+    public Task(int number) {
+        this.number = number;
+    }
+
+    public void run() {    //exact signature
+        System.out.print("\n Task " + number + " Started ");
+
+        for(int i = number*100; i <= (number*100) + 99; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n Task " + number + " Done");
+    }
+}
+
 public class ExecutorServiceTest {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+//        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        executorService.execute(new Task1());
-        //Task2 can only start when Task1 terminates
-        executorService.execute(new Thread(new Task2()));
+        executorService.execute(new Task(1));
+        //Two threads are now running at the same time
+        executorService.execute(new Thread(new Task(2)));
 
         //Task 3: 301 to 399
         System.out.print("\n Task3 Kicked Off ");
