aeb927c4d8e28cdd84881c66910438f6cb4cf9a3
Maria <maria.senina15@gmail.com>
2022-01-10 18:36:55 -0500

    Threads: ExecutorService returning values using Callable

---
+++ src/main/java/com/senina/maria/java/threadsAndConcurrency/CallableTest.java
@@ -0,0 +1,32 @@
+package com.senina.maria.java.threadsAndConcurrency;
+
+import java.util.concurrent.*;
+
+class CallableTask implements Callable<String> {
+    private String name;
+
+    public CallableTask(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(1000);
+        return "\nHello " + name;
+    }
+}
+
+public class CallableTest {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Future<String> welcomeFuture = executorService.submit(new CallableTask("Java"));
+        System.out.print("new CallableTask(\"Java\") executed");
+
+        String welcomeMessage = welcomeFuture.get();
+        System.out.println(welcomeMessage);
+
+        executorService.shutdown();
+        System.out.print("\nMain method completed");
+    }
+}
