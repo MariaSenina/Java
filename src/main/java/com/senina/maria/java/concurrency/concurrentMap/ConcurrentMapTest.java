cbfedaf37b2f922868a4377343d25bbcde5d98da
Maria <maria.senina15@gmail.com>
2022-01-12 17:32:25 -0500

    Concurrency: ConcurrentHashMap and computeIfAbsent method

---
+++ src/main/java/com/senina/maria/java/concurrency/concurrentMap/ConcurrentMapTest.java
@@ -0,0 +1,23 @@
+package com.senina.maria.java.concurrency.concurrentMap;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.LongAdder;
+
+public class ConcurrentMapTest {
+    public static void main(String[] args) {
+        ConcurrentMap<Character, LongAdder> occurrences = new ConcurrentHashMap<>();
+
+        String string = "ABCD ABCD ABCD";
+        for (char character : string.toCharArray()) {
+            occurrences.computeIfAbsent(character, ch -> new LongAdder()).increment();
+        }
+        System.out.println(occurrences);
+    }
+
+    //LongAdder provides atomic increment
+    //ConcurrentMap offers all the typical Map operations as well as computeIfAbsent method.
+    //computeIfAbsent method takes a key and a lambda expression
+
+    //NOTE: Local variables are NOT shared between threads
+}
