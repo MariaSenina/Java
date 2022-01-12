2d445ae6d42592114a94522b9fcff46973ba0479
Maria <maria.senina15@gmail.com>
2022-01-12 16:36:23 -0500

    Concurrency: Lock basics

---
+++ src/main/java/com/senina/maria/java/concurrency/BiCounterWithLock.java
@@ -0,0 +1,34 @@
+package com.senina.maria.java.concurrency;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BiCounterWithLock {
+    private int i = 0;
+    private int j = 0;
+
+    Lock lockForI = new ReentrantLock();
+    Lock lockForJ = new ReentrantLock();
+
+    public void incrementI() {
+        lockForI.lock(); //get lock for 'i'. Checks if any other thread has the lpckForI, if not, it takes the Lock. If yes, it waits.
+        i++;
+        lockForI.unlock(); //release lock for i
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void incrementJ() {
+        lockForJ.lock(); //get lock for j
+        j++;
+        lockForJ.unlock(); //release lock for j
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    //Locks help us break the synchronized code into chunks
+}
