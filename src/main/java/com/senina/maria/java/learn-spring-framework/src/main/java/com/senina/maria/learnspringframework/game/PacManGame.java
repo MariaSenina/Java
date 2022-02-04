6c7994c534c7d3889e4ba1e7f66dd6783a9729b0
Maria <maria.senina15@gmail.com>
2022-02-04 18:16:25 -0500

    Exercise: PacManGame Primary Component

--- src/main/java/com/senina/maria/java/learn-spring-framework/src/main/java/com/senina/maria/learnspringframework/game/PacManGame.java
+++ src/main/java/com/senina/maria/java/learn-spring-framework/src/main/java/com/senina/maria/learnspringframework/game/PacManGame.java
@@ -4,21 +4,26 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SuperContraGame implements GamingConsole {
+@Primary
+public class PacManGame implements GamingConsole {
 
+    @Override
     public void up() {
-        System.out.println("up");
+        System.out.println("Go up");
     }
 
+    @Override
     public void down() {
-        System.out.println("down");
+        System.out.println("Go down");
     }
 
+    @Override
     public void left() {
-        System.out.println("left");
+        System.out.println("Go left");
     }
 
+    @Override
     public void right() {
-        System.out.println("right");
+        System.out.println("Go right");
     }
 }
