62e7dbff91ee1f259e63a8fb31265fb66a9bf07c
Maria <maria.senina15@gmail.com>
2022-04-20 17:42:57 -0400

    Add GetMapping for 'id' PathVariable

--- src/main/java/com/senina/maria/java/learn-spring-framework/src/main/java/com/senina/maria/learnspringframework/courses/controller/CourseController.java
+++ src/main/java/com/senina/maria/java/learn-spring-framework/src/main/java/com/senina/maria/learnspringframework/courses/controller/CourseController.java
@@ -4,9 +4,11 @@ import com.senina.maria.learnspringframework.courses.bean.Course;
 import com.senina.maria.learnspringframework.courses.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
@@ -18,8 +20,13 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
-    @GetMapping("/courses/1")
-    public Course getFirstCourse() {
-        return getAllCourses().get(0);
+    @GetMapping("/courses/{id}")
+    public Course getFirstCourse(@PathVariable long id) {
+        Optional<Course> course = courseRepository.findById(id);
+        if(course.isEmpty()) {
+            throw new RuntimeException("Course not found " + id);
+        }
+
+        return course.get();
     }
 }
